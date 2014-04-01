b544db4fdd0e6709aa5cdda735809ee89c4b2277
Marko Nikolovski <mz.nikolovski@gmail.com>
2014-03-31 21:44:09 -0700

    rails init, gemfile update, gitignore, misc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeluciaCapstone::Application.config.secret_key_base = '9c0eca53e348a42f0c5bb6f23c9741bacf689bfaa202f9e66bf6369400c4a0e06a3c53df9054fe47933758a01c4bcb71e85ff141d2bc7040ee1c7f1f52e2aba5'
